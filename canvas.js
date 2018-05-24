e3dd0e7305641a95d9d19425425b90bbf7053b73
Unknown <info@markridder.nl>
2018-05-24 17:12:50 +0200

    Initial upload
    Halfway through the first setup

---
+++ canvas.js
@@ -0,0 +1,23 @@
+var w, width, h, height;
+var canvas;
+
+function createCanvas(canvas_name) {
+   canvas = document.createElement('canvas');
+   var body = document.querySelector('body');
+   canvas.setAttribute("id", canvas_name);
+   canvas.style.position = "absolute";
+   canvas.style.left = "0px";
+   canvas.style.right = "0px";
+   body.appendChild(canvas);
+   var ctx = getContext('2d');
+   resize();
+   window.addEventListener("resize", resize, false);
+   return ctx;
+}
+
+function resize(){
+   var c = document.getElementsByTagName('canvas');
+   width = w = window.innerWidth;
+   height = h = window.innerHeight;
+
+}
